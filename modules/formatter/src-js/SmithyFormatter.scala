68a200a7f06688f8bdf3f0181a296294abf9c377
Yisraelu <ysrlunion@gmail.com>
2022-11-17 10:27:20 -0500

    headers added

--- modules/formatter/src-js/SmithyFormatter.scala
+++ modules/formatter/src-js/SmithyFormatter.scala
@@ -1,7 +1,22 @@
+/* Copyright 2022 Disney Streaming
+ *
+ * Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package scala_js
 
-import smithytranslate.parsers.SmithyParserLive
-import smithytranslate.writer.Writer.WriterOps
+import smithytranslate.formatter.parsers.SmithyParserLive
+import smithytranslate.formatter.writers.IdlWriter.idlWriter
+import smithytranslate.formatter.writers.Writer.WriterOps
 
 import scala.scalajs.js.annotation._
 
