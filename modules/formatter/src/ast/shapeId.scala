2c2e676589f86ddf16f5342b6e09ae886af28cf0
David Francoeur <dfrancoeur04@gmail.com>
2022-12-22 13:35:52 -0500

    Use Ordering.by

--- modules/formatter/src/ast/shapeId.scala
+++ modules/formatter/src/ast/shapeId.scala
@@ -25,18 +25,11 @@ case class RootShapeId(either: Either[AbsoluteRootShapeId, Identifier])
 
 case class AbsoluteRootShapeId(nameSpace: Namespace, identifier: Identifier)
 object AbsoluteRootShapeId {
-  implicit val ord: Ordering[AbsoluteRootShapeId] =
-    new Ordering[AbsoluteRootShapeId] {
-      import writers.Writer._
-      import writers.ShapeIdWriter._
-      override def compare(
-          x: AbsoluteRootShapeId,
-          y: AbsoluteRootShapeId
-      ): Int = {
-        Ordering[String].compare(x.write, y.write)
-      }
-
-    }
+  implicit val ord: Ordering[AbsoluteRootShapeId] = {
+    import writers.Writer._
+    import writers.ShapeIdWriter._
+    Ordering.by(_.write)
+  }
 }
 
 case class Namespace(identifier: Identifier, suffix: List[Identifier])
