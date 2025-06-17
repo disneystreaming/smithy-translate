6ca3361e6f2aecc3b312cd8e47beb9ba3223fbdd
Antonio Morales <Antonio.Morales.-ND@disney.com>
2025-06-17 14:18:40 -0700

    fix headers

--- modules/json-schema/tests/src/TimeTypeSpec.scala
+++ modules/json-schema/tests/src/TimeTypeSpec.scala
@@ -1,4 +1,4 @@
-/* Copyright 2025 Disney Streaming
+/* Copyright 2022 Disney Streaming
  *
  * Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
