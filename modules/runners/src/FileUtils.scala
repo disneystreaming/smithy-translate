305ce8ca371dbe83ac320815d05a9d9322d855d6
yisraelU <ysrlunion@gmail.com>
2025-08-27 17:08:33 -0400

    mostly mechanical refactor to decouple the runners from the cli interface and the opts, facilitating reuse of runners in various contexts  and allowing to be published for scala3 too

---
+++ modules/runners/src/FileUtils.scala
@@ -0,0 +1,49 @@
+/* Copyright 2022 Disney Streaming
+ *
+ * Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://disneystreaming.github.io/TOST-1.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package smithytranslate
+package runners
+import cats.data
+import cats.data.NonEmptyList
+import smithytranslate.compiler.FileContents
+
+object FileUtils {
+  def readAll(
+      paths: NonEmptyList[os.Path],
+      includedExtensions: List[String]
+  ): List[FileContents] = {
+    paths.toList.flatMap { path =>
+      if (os.isDir(path)) {
+        val files = os
+          .walk(path)
+          .filter(p => includedExtensions.contains(p.ext))
+        files.map { in =>
+          FileContents(pathToNel(in, path), os.read(in))
+        }.toList
+      } else {
+        List(FileContents(NonEmptyList.of(path.last), os.read(path)))
+      }
+    }
+  }
+
+  def pathToNel(in: os.Path, path: os.Path): NonEmptyList[String] = {
+    val subParts = in
+      .relativeTo(path)
+      .segments
+      .toList
+    val baseNs = path.segments.toList.lastOption.toList
+    NonEmptyList.fromListUnsafe(baseNs ++ subParts)
+  }
+
+}
