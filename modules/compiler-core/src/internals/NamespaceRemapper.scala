524d92246cd98e7c0565f1665c0cc16d68c1e8e7
Jacob Barber <jacob.barber@disney.com>
2025-09-02 13:34:15 -0500

    Create headers

--- modules/compiler-core/src/internals/NamespaceRemapper.scala
+++ modules/compiler-core/src/internals/NamespaceRemapper.scala
@@ -1,4 +1,4 @@
-/* Copyright 2025 Disney Streaming
+/* Copyright 2022 Disney Streaming
  *
  * Licensed under the Tomorrow Open Source Technology License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,26 +12,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package smithytranslate.compiler
-package internals
 
-import cats.data.Chain
-import cats.data.NonEmptyChain
-
-final class NamespaceRemapper(
-    remaps: Map[NonEmptyChain[String], Chain[String]]
-) {
-  final def remap(ns: List[String]): List[String] = {
-    remaps
-      .collectFirst {
-        case (key, value) if ns.startsWith(key.toChain.toList) =>
-          (value ++ Chain.fromSeq(ns.drop(key.length.toInt))).toList
-      }
-      .getOrElse(ns)
-  }
-
-  final def remap(defId: DefId): DefId = {
-    defId.copy(namespace = Namespace(remap(defId.namespace.segments)))
-  }
-
-}
